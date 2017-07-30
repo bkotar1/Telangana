06ac2de43af5de1e3f971b729e594794dd96dd7d
srikanth balagoni <srikanth.balagoni@capitalone.com>
2017-07-29 19:04:42 -0500

    Boiler Plate

---
+++ App/Navigation/AppNavigation.js
@@ -0,0 +1,23 @@
+import { StackNavigator } from 'react-navigation'
+import LaunchScreen from '../Containers/LaunchScreen'
+import TelanagaDataScreen from '../Containers/TelanaganDataScreen'
+
+import styles from './Styles/NavigationStyles'
+
+// Manifest of possible screens
+const PrimaryNav = StackNavigator(
+  {
+    LaunchScreen: { screen: LaunchScreen },
+    TelanagaDataScreen: { screen: TelanagaDataScreen }
+  },
+  {
+    // Default config for all screens
+    headerMode: 'none',
+    initialRouteName: 'TelanagaDataScreen',
+    navigationOptions: {
+      headerStyle: styles.header
+    }
+  }
+)
+
+export default PrimaryNav
