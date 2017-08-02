af28211f9681e15df0815fe090e7bf149f27aac3
srikanth balagoni <srikanth.balagoni@capitalone.com>
2017-08-02 17:24:37 -0500

    Click Event with District ID works

--- App/Containers/TelanganaMapScreen.js
+++ App/Containers/TelanganaMapScreen.js
@@ -5,19 +5,49 @@ import { Card } from "react-native-material-ui";
 
 
 
+
 export default class TelanganaMapScreen extends Component {
-  showArea() {
-    alert("clickable");
-  }
+  constructor(props) {
+        super(props)
+        this.postMessage = this.postMessage.bind(this)
+    }
+      receivedMessage(action) {
+        console.log('received message from web view')
+        console.log(action)
+    }
+    postMessage() {
+        console.log('sending message from react native')
+        this.webView.postMessage('message from react native')
+    }
+  
 
   render() {
-    return (
+    function processMessage(e) {
+      alert(e.nativeEvent.data);
+    }
+  /**  return (
       <View style={{flex: 1,alignItems:'center',justifyContent:'center',backgroundColor:'red'}}>
-        <WebView
+        <WebView  javaScriptEnabled={true} scalesPageToFit={false} onMessage={this.showArea()}
           style={{width:600,height:600,justifyContent:'center',alignItems:'center'}}
           source={require('./TelanganaMapSvg.html')}
+          ref={x => {
+                        this.webView = x
+                    }}
         />
+        
       </View>
     );
+    */
+
+    return (
+      <View style={{flex: 1,alignItems:'center',justifyContent:'center',backgroundColor:'red'}}>
+        <WebView javaScriptEnabled={true} scalesPageToFit={false} style={{width:600,height:600,justifyContent:'center',alignItems:'center'}}
+                    source={require('./TelanganaMapSvg.html')}
+                    onMessage={processMessage}
+                />
+                
+      </View>
+    );
+  
   }
 }
--- android/app/src/main/java/com/telangana/MainApplication.java
+++ android/app/src/main/java/com/telangana/MainApplication.java
@@ -3,6 +3,7 @@ package com.telangana;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new WebViewBridgePackage(),
             new RNDeviceInfo(),
             new ReactNativeConfigPackage(),
             new ReactNativeI18n(),
